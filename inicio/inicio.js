b1896719ffb8b5f442d07ecea810214ccbd2e219
Anderson Carreño <aszc178@gmail.com>
2024-03-13 15:34:05 -0500

    ..

--- inicio/inicio.js
+++ inicio/inicio.js
@@ -23,3 +23,36 @@ function nextSlide() {
 
 showSlide(currentIndex);
 setInterval(nextSlide, 3000);
+
+// PARALLAX
+
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+        document.querySelectorAll('.parallax-section').forEach(function(section) {
+            const speed = -0.6;
+            const sectionTop = section.offsetTop - window.pageYOffset;
+            const moveAmount = sectionTop * speed;
+            section.style.transform = 'translateY(' + moveAmount + 'px)';
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.transform = 'scale(1.2)';
+                entry.target.style.transition = 'transform 1s ease-in-out';
+            } else {
+                entry.target.style.transform = 'scale(1)';
+            }
+        });
+    }, {
+        threshold: 0.1 // Este valor determina qué porcentaje de la imagen debe estar visible para activar el zoom.
+    });
+
+    document.querySelectorAll('.imagenes-inicio img').forEach(img => {
+        observer.observe(img);
+    });
+});
