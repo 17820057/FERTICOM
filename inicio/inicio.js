552c30db2087ed5d5b74b926e23ed73502f34c84
Anderson Carreño <aszc178@gmail.com>
2024-03-14 14:07:20 -0500

    ..

--- inicio/inicio.js
+++ inicio/inicio.js
@@ -2,24 +2,20 @@
 
 const sliderInicio = document.querySelector('.sliderinicio');
 const slides = sliderInicio.querySelectorAll('li');
-// const totalSlides = slides.length;
 let currentIndex = 0;
 
 function showSlide(index) {
     slides.forEach(slide => {
         slide.style.opacity = 0;
         slide.style.zIndex = 0;
-        // slide.style.display = 'none';
     });
 
     slides[index].style.opacity = 1;
     slides[index].style.zIndex = 1;
-    // slides[index].style.display = 'block';
 }
 
 function nextSlide() {
     currentIndex = (currentIndex + 1) % slides.length;
-    // currentIndex = (currentIndex + 1) % totalSlides;
     showSlide(currentIndex);
 }
 
