aebf018ce02cf4e75a3c36076ebfac285092e5c6
Anderson Carreño <aszc178@gmail.com>
2024-03-19 06:23:47 -0500

    ..

--- inicio/inicio.js
+++ inicio/inicio.js
@@ -122,27 +122,20 @@ if (video.readyState >= 2) {
 
 // SLIDER AUTOMATICO VERTICAL FINAL INICIO
 
-// const sliderVertical = document.querySelector('.slider-vertical');
-// const slides = sliderInicio.querySelectorAll('li');
-// let currentIndex = 0;
-
-// function showSlide(index) {
-//     slides.forEach(slide => {
-//         slide.style.opacity = 0;
-//         slide.style.zIndex = 0;
-//     });
-
-//     slides[index].style.opacity = 1;
-//     slides[index].style.zIndex = 1;
-// }
-
-// function nextSlide() {
-//     currentIndex = (currentIndex + 1) % slides.length;
-//     showSlide(currentIndex);
-// }
-
-// showSlide(currentIndex);
-// setInterval(nextSlide, 3000);
+document.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelectorAll('.slider-vertical .slide');
+    let currentSlide = 0;
+  
+    function goToNextSlide() {
+      slides[currentSlide].classList.remove('active-slide');
+      currentSlide = (currentSlide + 1) % slides.length;
+      slides[currentSlide].classList.add('active-slide');
+    }
+  
+    slides[currentSlide].classList.add('active-slide');
+    setInterval(goToNextSlide, 3000);
+  });
+  
 
   
   
